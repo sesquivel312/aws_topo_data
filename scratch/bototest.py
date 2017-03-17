5bc3fa99ae573d4b504df00dc3a0ce30ab91e6ee
Steve <retail.mbox@mgail.com>
2017-03-17 14:40:16 -0700

    complete successful test against proxy with auth

--- scratch/bototest.py
+++ scratch/bototest.py
@@ -1,29 +1,49 @@
+#!/usr/bin/env python
 import sys
 import getpass
 import os
 import pprint as pp
 
-mybotocore = '/home/steve/dev/botocore'
-myboto3 = '/home/steve/dev/boto3'
+FLAG_URL_ENCODE_USER_INFO = False # url encode the username and password?
+FLAG_PROMPT_FOR_PROXY = False  # set True to prompt user for proxy user and pwd
 
-def get_path_index(list):
-    for i, v in enumerate(list):
-        if v.startswith('/usr'):
-            return i
+PROXY_HOST = 'ats01'
+PROXY_PORT = '8080'
+BOTO_CORE = '/home/steve/dev/botocore'
+BOTO_BOTO3 = '/home/steve/dev/boto3'
+CA_BUNDLE = '/home/steve/certs/ec2us-west-2amazonawscom.crt'
 
-i = get_path_index(sys.path)
+sys.path.insert(0, BOTO_CORE)
+sys.path.insert(0, BOTO_BOTO3)
 
-sys.path.insert(i, mybotocore)
-sys.path.insert(i, myboto3)
-os.environ['REQUESTS_CA_BUNDLE'] = '/home/steve/certs/owasp_zap_root_ca.cer'
-print 'ca bundle envar: ', os.environ['REQUESTS_CA_BUNDLE']
 
 import boto3
 
-print 'boto3 location: ', boto3.__file__
+os.environ['REQUESTS_CA_BUNDLE'] = CA_BUNDLE
 
 kid = getpass.getpass('AWS KID: ')
 sk = getpass.getpass('AWS Secret Key: ')
+
+if FLAG_PROMPT_FOR_PROXY:
+    proxy_host = getpass.getpass('Enter proxy host name: ')
+    proxy_port = getpass.getpass('Enter proxy port number: ')
+else:
+    proxy_host = PROXY_HOST
+    proxy_port = PROXY_PORT
+
+proxy_user = getpass.getpass('Enter proxy user ID: ')
+proxy_pwd = getpass.getpass('Enter proxy pwd: ')
+
+if proxy_user and proxy_port:  # deal with proxy auth user info
+    if FLAG_URL_ENCODE_USER_INFO:
+        user_info = urllib.quote('{}:{}'.format(proxy_user, proxy_pwd))
+    else:
+        user_info = '{}:{}'.format(proxy_user, proxy_pwd)
+    proxy_url = 'https://{}@{}:{}'.format(user_info, proxy_host, proxy_port)
+else:  # no proxy auth info
+    proxy_url = 'https://{}:{}'.format(proxy_host, proxy_port)
+
+os.environ['https_proxy'] = proxy_url
 
 s = boto3.session.Session(aws_access_key_id=kid, aws_secret_access_key=sk, region_name='us-west-2')
 ec2 = s.resource('ec2')
--- scratch/scratch.py
+++ scratch/scratch.py
@@ -1,24 +1,43 @@
+#!/usr/bin/env python
+
 import getpass
 import urllib
+import os
 
-import boto3
+import requests
+
+FLAG_TEST_REQUESTS = True  # change to false to test Boto's requests
+FLAG_URL_ENCODE_USER_INFO = False # url encode the username and password?
+
+
+REQUESTS_ENDPOINT = 'https://www.cisco.com'
+PROXY_HOST = 'ats01'
+PROXY_PORT = '8080'
+CA_BUNDLE_PATH = '/home/steve/certs/ec2us-west-2amazonawscom.crt'
 
 kid = getpass.getpass('Enter key ID: ')
-skey = getpass.getpass('Enter sec. key: ')
+sec_key = getpass.getpass('Enter sec. key: ')
 uid = getpass.getpass('Enter proxy uid: ')
 pwd = getpass.getpass('Enter proxy pwd: ')
 
-userinfo = urllib.quote('{}:{}'.format(uid, pwd))
+if FLAG_URL_ENCODE_USER_INFO:
+    user_info = urllib.quote('{}:{}'.format(uid, pwd))
+else:
+    user_info = '{}:{}'.format(uid, pwd)
 
-proxy_url = 'https://{}@localhost:8080'.format(userinfo)
+proxy_url = 'https://{}@{}:{}'.format(user_info, PROXY_HOST, PROXY_PORT)
 
 proxies = {'https': proxy_url}
 
-s = boto3.session.Session(aws_access_key_id=kid, aws_secret_access_key=skey, region_name='us-west-2')
-ec2 = s.resource('ec2')
+os.environ['HTTPS_PROXY'] = proxy_url
 
-vpcs = ec2.vpcs.all()
+if FLAG_TEST_REQUESTS:
+    r = requests.get(REQUESTS_ENDPOINT, verify=CA_BUNDLE_PATH)
+else:
+    s = boto3.session.Session(aws_access_key_id=kid, aws_secret_access_key=skey, region_name='us-west-2')
+    ec2 = s.resource('ec2', verify=CA_BUNDLE_PATH)
 
-for i in vpcs:
-    print i.id
-    break
+    vpcs = ec2.vpcs.all()
+
+    for i in vpcs:
+        print i.id
