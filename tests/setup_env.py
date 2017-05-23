e9524050974a91db32b79b8f29c23bf55750fbc3
steve <retail.mbox@gmail.com>
2017-05-23 10:28:54 -0700

    added script to setup environment - primarily for testing

---
+++ tests/setup_env.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import os
+import getpass
+import argparse
+
+# check for environment variables
+# prompt for AWS creds
+# prompt for ca bundle
+# prompt for proxy (w/default)
+# add environment variables
+#   aws creds
+#   ca bundle
+#   
