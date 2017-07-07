a3bc3c45165ba495fba7dab13b5b978677930f7b
steve <retail.mbox@gmail.com>
2017-07-06 17:51:34 -0700

    completed aws account switch helper script that uses temp creds from AWS CLI get-session-token command

--- misc/aws_acct_switch.sh
+++ misc/aws_acct_switch.sh
@@ -37,60 +37,64 @@
 #
 # Notes:
 #
-#   - This script will likely be run once for each account before the per-account session-tokens exist
-#     and again multiple times per account after per account session token variables are set/exported - b/c
-#     the per account key-id and secret-key variables are necessary to run the aws commands to generate the
-#     session tokens, once obtained they can be set, per account, and this script can be run again to change to that
-#     account.  When this is done the key-id and secret-key values will change from the API keys to the STS issued
-#     temporary credentials
+#   - If the user enters the token code right before it expires it's possible they'll need to run again as
+#     the code could expire between time code is collected and the aws CLI command is run
 #
 #   - Of course you can add as many accounts as you like, you may want to add some hierarchy to the
 #     the prefixes - if you have personal and work accounts you want to act on simultaneously.  For example, if
 #     you have work dev and personal dev, maybe you use the prefixes wkdev and mydev, respectively
 #
-#   - Actual account prefixes aren't used here in order to prevent putting any "personally identifiable information"
-#     into GitHub
-#
-#   - To set the session tokens per account use the following commands to setup the session token variables
-#     for each account.
-#
-#               read <acctpfx>_{id,key,token} <<< $(aws sts get-session-token --serial-number ${<acctpfx>_mfa} \
-#               --token-code <current_MFA_code> | <path>/<to>/parse_aws_tmp_creds.py); export <acctpfx>_{id,key,token}
-#
-#     This command line says:
-#
-#       - Get a session token from AWS CLI for the mfa device with serial-number $<acctpfx>_mfa
-#       - Parse that output (it's JSON) using the .py script - which will return (via STDIN) 3 values
-#         separated by spaces (or whatever $IFS is set to), those are the values of the AWS key-id, key, session token
-#       - send the parsed result to "read" which will set the shell variables named <acctpfx>_(id,key,token) to
-#         the parsed values - in order given
-#       - then export the shell variables (i.e. make them environment variables) w/the same names
-#
-#   - Given all the above, the way you'd use this script is:
-#
-#       1 customize misc/aws_acct_switch.sh for your accounts - SAVE IT OUTSIDE GIT'S SCOPE
-#       2 export the id, key and mfa-serial-number variables per account
-#       3 use aws_acct_switch.sh to switch to some account using an acct prefix as the argument
-#       4 run the command above, i.e. read ...
-#       5 repeat 3-4 for each account
-#       6 you can now use aws_acct_switch.sh to switch to any account and the temp credentials will be used
+#   - Setups up per-account variables AND per-account session variables; the latter hold the temp creds; the
+#     session versions are prefixed like so: <acctpfx>_s_{id,key,token}
 #
 #   - This script is most useful if your accounts don't change much (since you have to keep ending it in if they do)
 
-# todo P2 read the account prefixes from a file?
-# todo P1 code (steps 1-6) - likely need to have separate per account cred and "temp cred" variables
+# todo P2 parameterize this so code isn't repeated - possibly read prefixes from file
 
 case "$1" in
-    <acctprx1>)
-        export AWS_ACCESS_KEY_ID=$<acctpfx1>_id
-        export AWS_SECRET_ACCESS_KEY=$<acctpfx1>_key
-        export AWS_SESSION_TOKEN=$<acctpfx1>_token
+    <acctpfx1>)  # full(er) account name
+
+        #export the permanent keys
+        export AWS_ACCESS_KEY_ID=${<acctpfx1>_id}
+        export AWS_SECRET_ACCESS_KEY=${<acctpfx1>_key}
+    
+        # if don't already have session vars, create them
+        if [[ -z ${<acctpfx1>_s_token} ]]  # check no session token yet
+            then
+                read -p "Enter MFA Code: " TCODE  # prompt for and store MFA token code
+                read <acctpfx1>_s_{id,key,token} <<< \  # read the session id, key and token values into variables
+                    $(aws sts get-session-token --serial-number ${<acctpfx1>_mfa} \  # using AWS CLI & expanding vars
+                    --token-code ${TCODE} | ~/dev/aws_topo_data/misc/parse_aws_tmp_creds.py)
+                export <acctpfx1>_s_{id,key,token}  # make environment vars so available in child shells
+        fi
+    
+        # we have session variables, set the AWS variables to those values
+        export AWS_ACCESS_KEY_ID=${<acctpfx1>_s_id}
+        export AWS_SECRET_ACCESS_KEY=${<acctpfx1>_s_key}
+        export AWS_SESSION_TOKEN=${<acctpfx1>_s_token}
+
         ;;
 
-    <acctprx2>)
-        export AWS_ACCESS_KEY_ID=$<acctpfx2>_id
-        export AWS_SECRET_ACCESS_KEY=$<acctpfx2>_key
-        export AWS_SESSION_TOKEN=$<acctpfx2>_token
+    <acctpfx2>)  # full(er) account name
+
+        #export the permanent keys
+        export AWS_ACCESS_KEY_ID=${<acctpfx2>_id}
+        export AWS_SECRET_ACCESS_KEY=${<acctpfx2>_key}
+
+        # if don't already have session vars, create them
+        if [[ -z ${<acctpfx2>_s_token} ]]
+            then
+                read -p "Enter MFA Code: " TCODE
+                read <acctpfx2>_s_{id,key,token} <<< \
+                    $(aws sts get-session-token --serial-number ${<acctpfx2>_mfa} \
+                    --token-code ${TCODE} | ~/dev/aws_topo_data/misc/parse_aws_tmp_creds.py)
+                export <acctpfx2>_s_{id,key,token}
+        fi
+
+        # we have session variables, set the AWS variables to those values
+        export AWS_ACCESS_KEY_ID=${<acctpfx2>_s_id}
+        export AWS_SECRET_ACCESS_KEY=${<acctpfx2>_s_key}
+        export AWS_SESSION_TOKEN=${<acctpfx2>_s_token}
 
         ;;
 
